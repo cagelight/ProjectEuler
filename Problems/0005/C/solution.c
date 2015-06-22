d144cb9834caa0da20aaf522369f73bd45ac68ad
Kallikrates <kallikrates.koios@gmail.com>
2015-06-22 05:32:53 -0700

    Minor C #5 cleanup

--- Problems/0005/C/solution.c
+++ Problems/0005/C/solution.c
@@ -1,12 +1,9 @@
-#include <stdlib.h>
 #include <stdint.h>
-#include <stdio.h>
-#include <stdbool.h>
 
 extern uint64_t project_euler_c_solution() {
 	uint64_t res = 1;
 	uint8_t ccur = 2;
-	while(true) {
+	while(1) {
 		if (ccur > 20) break;
 		if (res % ccur == 0) {
 			ccur++;
